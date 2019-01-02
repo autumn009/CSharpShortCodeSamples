1e6e57c8d322b838bad659933633ebdae1373cc1
John Hilts <johnhilts@live.com>
2019-01-01 23:16:55 -0800

    NEWCODE2: 12 lines, 313 bytes

---
+++ sample3/chap4/配列の値が繰り返される値で初期化された配列/NEWCODE2/NEWCODE2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        const int repeatCount = 10;
+        int[] baseAarray = { 2, 5, 1 };
+	var ar = Enumerable.Range(1, repeatCount).SelectMany(x => baseAarray).ToArray();
+        foreach (var item in ar) System.Console.Write($"{item},");
+    }
+}
